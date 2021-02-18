80ba8051f70a710711ea78415596926a2878c089
mcervantes71 <cervantes.martine@gmail.com>
2021-02-18 16:09:41 -0700

    Add json render to pokemons info controller

--- app/controllers/pokemon_info_controller.rb
+++ app/controllers/pokemon_info_controller.rb
@@ -2,6 +2,11 @@ class PokemonInfoController < ApplicationController
   # GET /pokemon_info/1 or /pokemon_info/1.json
   def show
     @pokemon = Pokemon.find(params[:id])
-    @pokemon_info = PokemonInfo.find(params[:id])
+    @pokemon_info = PokemonInfo.select(:id, :height, :weight, :moves, :abilities, :stats).find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @pokemon_info }
+     end
   end
 end
